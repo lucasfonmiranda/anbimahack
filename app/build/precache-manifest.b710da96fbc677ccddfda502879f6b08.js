6448606d8723f126d410f8ec706d01fc34194caf
BrunoFFari <bruno27ffarias@gmail.com>
2019-09-01 13:19:58 -0300

    add build dashboard

---
+++ app/build/precache-manifest.b710da96fbc677ccddfda502879f6b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311b3bca8cead8f1114ed272f50dbce5",
+    "url": "index.html"
+  },
+  {
+    "revision": "f7b0dc88352f29aeadd1",
+    "url": "static/css/main.9aee624e.chunk.css"
+  },
+  {
+    "revision": "f6aceeaeac25224999a1",
+    "url": "static/js/2.5b171100.chunk.js"
+  },
+  {
+    "revision": "f7b0dc88352f29aeadd1",
+    "url": "static/js/main.f8fcc9e9.chunk.js"
+  },
+  {
+    "revision": "f40b12b31de902f08464",
+    "url": "static/js/runtime~main.f923b075.js"
+  }
+]);
